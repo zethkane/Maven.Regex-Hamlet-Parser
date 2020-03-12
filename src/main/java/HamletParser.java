610c6f194b4e3d344ca9f7522565c94a379ecdc8
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-03-12 15:52:35 -0400

    finshed

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -34,6 +36,46 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String changeHamletToLeon(){
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        return matcher.replaceAll("Leon");
+    }
+
+    public String changeHoratioToTariq(){
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        return matcher.replaceAll("Tariq");
+    }
+
+    public void findHoratio(){
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        for (int i = 0; matcher.find(); i++) {
+            System.out.println(new StringBuilder()
+            .append("\n-------------------")
+            .append("\nValue = " + matcher.group())
+             .append("\nMatch Number = " + i)
+             .append("\nStarting index = " + matcher.start())
+             .append("\nEnding index = " + matcher.end())
+            .toString());
+        }
+    }
+
+    public void findHamlet(){
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        for (int i = 0; matcher.find(); i++) {
+            System.out.println(new StringBuilder()
+                    .append("\n-------------------")
+                    .append("\nValue = " + matcher.group())
+                    .append("\nMatch Number = " + i)
+                    .append("\nStarting index = " + matcher.start())
+                    .append("\nEnding index = " + matcher.end())
+                    .toString());
+        }
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +19,53 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletText);
+        String expected =matcher.replaceAll("Leon");
+        String actual = hamletParser.changeHamletToLeon();
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletText);
+        String expected =matcher.replaceAll("Tariq");
+        String actual = hamletParser.changeHoratioToTariq();
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletText);
+        for (int i = 0; matcher.find(); i++) {
+            System.out.println(new StringBuilder()
+                    .append("\n-------------------")
+                    .append("\nValue = " + matcher.group())
+                    .append("\nMatch Number = " + i)
+                    .append("\nStarting index = " + matcher.start())
+                    .append("\nEnding index = " + matcher.end())
+                    .toString());
+        }
+
+        String actual = hamletParser.changeHamletToLeon();
+
     }
 
     @Test
     public void testFindHamlet() {
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletText);
+        for (int i = 0; matcher.find(); i++) {
+            System.out.println(new StringBuilder()
+                    .append("\n-------------------")
+                    .append("\nValue = " + matcher.group())
+                    .append("\nMatch Number = " + i)
+                    .append("\nStarting index = " + matcher.start())
+                    .append("\nEnding index = " + matcher.end())
+                    .toString());
+        }
     }
 }
